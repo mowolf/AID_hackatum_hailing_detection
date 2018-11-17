a5fbaa2eb1bcc7e5da2962b4f73a6bcfcb4928a9
Martin Wudenka <Martin.Wudenka@gmx.de>
2018-11-17 10:51:56 +0100

    Clean up frontend dependencies

--- backend_frontend/src/App.js
+++ backend_frontend/src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from 'styled-components'
+
+const Background = styled.div`
+  background-color: #ecf0f1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Background>
+        Test
+      </Background>
     );
   }
 }
