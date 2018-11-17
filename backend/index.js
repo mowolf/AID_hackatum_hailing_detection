26c4c6d4b8084d91b412d471e734e2339129e18e
whateverforever <max@thousandyardstare.de>
2018-11-17 10:13:50 +0100

    added state and function to query it

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
 
+const state = {
+    carStates: [],
+    waitingPassengers: []
+};
+
 app.get("/", function(req, res) {
     res.send("Hello World!");
 });
+
+app.get("carStates", (req, res) => {});
 
 app.listen(3000, function() {
     console.log("Example app listening on port 3000!");
