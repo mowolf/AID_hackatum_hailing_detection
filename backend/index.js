ae8f97e8f656619145d9fa78141ce4a18a86f512
whateverforever <max@thousandyardstare.de>
2018-11-17 10:19:47 +0100

    return carStates

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ app.get("/", function(req, res) {
     res.send("Hello World!");
 });
 
-app.get("carStates", (req, res) => {});
+app.get("/carStates", (req, res) => {
+    res.json(state.carStates);
+});
 
 app.listen(3000, function() {
     console.log("Example app listening on port 3000!");
